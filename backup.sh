006ed78fbaa2931a01d99a9ade74031d455c6a34
Pavel Kondratenko <iam@kondratenko.me>
2018-05-02 14:57:18 +0300

    init

---
+++ backup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# function to display commands
+exe() { echo "\$ $@" ; "$@"; }
+
+# local configuration
+path="$(pwd)"
+
+# load backups variables
+export $(cat .env | xargs) || exit 1
+
+# creating variables
+sourceProjectsFolder="${BACKUP_PROJECTS_FOLDER}"
+sourceCloudFolder="${BACKUP_CLOUD_FOLDER}"
+sourceCustomFile="${BACKUP_CUSTOM_FILE}"
+
+targetFolder="${BACKUP_TARGET_FOLDER}"/"$(date +%Y-%m-%d)"
+targetFolderCustom="${targetFolder}"/custom
+targetFolderMysql="${targetFolder}"/mysql
+targetFolderCloud="${targetFolder}"/cloud
+
+# mysql configuration
+mysqlCmd=/usr/bin/mysql
+mysqlDumpCmd=/usr/bin/mysqldump
+mysqlUser="${BACKUP_MYSQL_USER}"
+mysqlPassword="${BACKUP_MYSQL_PASSWORD}"
+
+
+# creating backup folders
+echo "Creating target folders..."
+exe mkdir -p "${targetFolderCustom}"
+exe mkdir -p "${targetFolderMysql}"
+exe mkdir -p "${targetFolderCloud}"
+
+# clean target folder if not empty
+echo "Cleaning target folder if already not empty..."
+exe rm -rf ${targetFolderCustom}/*
+exe rm -rf ${targetFolderMysql}/*
+exe rm -rf ${targetFolderCloud}/*
+
+
+# stepping into the folder
+exe cd "${sourceProjectsFolder}"
+
+# looping the subfolders for custom dumping scrips
+for D in *; do
+    if [ -d "${D}" ]; then
+    	# running custom dump script
+        if [ -f "${D}"/"${sourceCustomFile}" ]; then
+        	pathToProjectSource="${sourceProjectsFolder}"/"${D}"
+        	pathToProjectTarget="${targetFolderCustom}"/"${D}"
+
+        	exe mkdir -p "${pathToProjectTarget}"
+        	exe cd "${pathToProjectSource}"
+        	exe sh "${sourceCustomFile}" "${pathToProjectTarget}"
+		fi
+    fi
+done
+
+
+# select databases list
+echo "Select allowed databases for user ${mysqlUser}..."
+databases=`$mysqlCmd -u$mysqlUser -p$mysqlPassword -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+
+# dump selected databases 
+echo "Dump all selected databases..."
+for db in $databases; do
+  echo "Dump database ${db}..."
+  $mysqlDumpCmd --force --opt --user=$mysqlUser -p$mysqlPassword --databases $db | gzip > "${targetFolderMysql}/$db.gz"
+done
+
+
+# backuping cloud (static files from one folder)
+echo "Backuping cloud from ${sourceCloudFolder}..."
+exe zip -r "${targetFolderCloud}"/cloud.zip "${sourceCloudFolder}"
+exe mv 
+
+
+echo "Complete!"
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Shell script to backup remote server on demand
+**Imprisoned for a specific task and weakly customized. Lies here not to get lost.**
+
+## Prerequirements
+```bash
+cp .env.example .env
+```
+
+## Configuration - variables in `.env` file
+**BACKUP_PROJECTS_FOLDER**=/home/user/www
+
+Path to projects with one level structure. Used to search custom backup scripts. For example:
+
+```
+- /home/webulla/www/webulla.ru
+- /home/webulla/www/landing.webulla.ru
+```
+
+**BACKUP_CLOUD_FOLDER**=/home/user/www/cloud.domain.ru/web
+
+Path to folder which is static files storage. Will be dumped as a zip archive.
+
+**BACKUP_TARGET_FOLDER**=/home/user/backups
+
+Where to save backups (will be created or cleaned).
+
+**BACKUP_CUSTOM_FILE**=backup.sh
+
+Files to execute in each top-level folder in **BACKUP_PROJECTS_FOLDER**. For example, in this example (backup.sh) the following commands will be executed.
+
+```bash
+sh /home/user/www/webulla.ru/backup.sh $BACKUP_TARGET_FOLDER
+sh /home/user/www/webulla.ru/backup.sh $BACKUP_TARGET_FOLDER
+```
+
+**BACKUP_MYSQL_USER**=root
+
+Mysql user to dump all databases for this user.
+
+**BACKUP_MYSQL_PASSWORD**=root
+
+Mysql password for **BACKUP_MYSQL_USER**.
